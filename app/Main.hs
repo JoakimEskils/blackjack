d6330d318bfb57af1c96a2086450fa969cadf2c2
Joakim Eskils <joakim.eskils@bricco.se>
2020-11-26 21:03:33 +0100

    Added Card datatype and its scores

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,35 @@
 module Main where
 
+data Card
+  = Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
+  deriving (Show, Eq, Enum)
+
+cardScore :: Card -> Word
+cardScore Ace = 1
+cardScore Two = 2
+cardScore Three = 3
+cardScore Four = 4
+cardScore Five = 5
+cardScore Six = 6
+cardScore Seven = 7
+cardScore Eight = 8
+cardScore Nine = 9
+cardScore Ten = 10
+cardScore Jack = 10
+cardScore Queen = 10
+cardScore King = 10
+
 main :: IO ()
 main = putStrLn "Hello from Haskell!"
